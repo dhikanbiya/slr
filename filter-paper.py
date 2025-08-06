c41a594e562586eb6de0ccf74171713abf4c2aa6
Dhika Anbiya <danbiya@mbp>
2025-08-06 13:35:37 +0200

    fixing prompt and add options for selecting LLM model

--- filter-paper.py
+++ filter-paper.py
@@ -1,9 +1,12 @@
 import pandas as pd
 from ollama import chat
 from ollama import ChatResponse
+from pathlib import Path
 
-def ollama_response(abstract):
-    prompt  = "I want you to check my paragraph whether is related to vulnerability detection or not. Answer only with YES or NO. Yes if its related and No if its not related"
+
+prompt = "I want you to check my paragraph whether is related to vulnerability detection or not. Answer only with YES or NO. YES if it is related and NO if it is not related, no need to write any comment or feedback."
+
+def ollama_response(abstract):    
     response: ChatResponse = chat(model='llama4:latest',messages=[
         {"role": "user", "content": prompt},
         {"role": "user", "content": abstract}
@@ -13,8 +16,7 @@ def ollama_response(abstract):
 
 
 def add_classification(df):
-    df = df[['Authors','Abstract','Title','Year']].copy()
-
+    df = df.copy()
     related = []
     
     for index,row in df.iterrows():
@@ -23,16 +25,19 @@ def add_classification(df):
     df['Related']=related
     return df
 
-def main(csv_file):
+def main(csv_file, llm_model):
     chunksize = 1000
     chunklist = []
 
     for chunk in pd.read_csv(csv_file, chunksize=chunksize):
         chunklist.append(chunk)    
     df = pd.concat(chunklist, ignore_index=True)
+
+    df = df.head(10)
     
     df = add_classification(df)
-    df.to_csv('result.csv', index=False)
+    filename = Path(csv_file).stem
+    df.to_csv(f'{filename}_{llm_model}_result.csv', index=False)
     print("saved")
 
 
@@ -40,6 +45,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='filter papers based on vulnerability detection relevance.')
     parser.add_argument('csv_file', type=str, help='Path to the input CSV file containing paper data.')
+    parser.add_argument('llm_model', type=str, default='llama4:latest', help='Model to use for Ollama chat.')
     args = parser.parse_args()
-    
-    main(args.csv_file)
+
+    main(args.csv_file, args.llm_model)
